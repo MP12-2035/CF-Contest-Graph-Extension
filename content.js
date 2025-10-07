5fea3895ef5bba2d18188f65021878641d6bb93a
Samridhi Roy <66419090+MP12-2035@users.noreply.github.com>
2025-10-07 10:00:06 +0530

    Create content.js

---
+++ content.js
@@ -0,0 +1,17 @@
+(async function() {
+    const div = document.createElement('div');
+    div.id = 'graphDiv';
+    div.style.width = '100%';
+    div.style.height = '600px';
+    document.body.prepend(div);
+
+    const urlParts = window.location.pathname.split('/');
+    const contestId = urlParts[2];
+
+    chrome.storage.local.get(['myHandle', 'friends'], async (res) => {
+        const myHandles = [res.myHandle, ...(res.friends || [])];
+        chrome.runtime.sendMessage({action: "fetchStandings", contestId}, response => {
+            plotGraph(response.users, myHandles);
+        });
+    });
+})();
