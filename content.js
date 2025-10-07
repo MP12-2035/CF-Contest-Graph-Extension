dc007c40e6207661a3255e2875908b6251a463a1
Samridhi Roy <66419090+MP12-2035@users.noreply.github.com>
2025-10-07 10:28:52 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,25 +1,27 @@
-// Inject Plotly from CDN
-const plotlyScript = document.createElement('script');
-plotlyScript.src = "https://cdn.plot.ly/plotly-latest.min.js";
-plotlyScript.onload = () => {
-    initGraph(); // your function that calls plotGraph()
-};
-document.head.appendChild(plotlyScript);
-
-function initGraph() {
+(function() {
+    // Create the graph container
     const div = document.createElement('div');
     div.id = 'graphDiv';
     div.style.width = '100%';
     div.style.height = '600px';
+    div.style.marginBottom = '20px';
     document.body.prepend(div);
 
+    // Get contest ID from URL
     const urlParts = window.location.pathname.split('/');
     const contestId = urlParts[2];
 
+    // Get user handles from storage
     chrome.storage.local.get(['myHandle', 'friends'], async (res) => {
         const myHandles = [res.myHandle, ...(res.friends || [])];
+
+        // Fetch contest standings from background.js
         chrome.runtime.sendMessage({action: "fetchStandings", contestId}, response => {
-            plotGraph(response.users, myHandles);
+            if (response && response.users && response.users.length > 0) {
+                plotGraph(response.users, myHandles);
+            } else {
+                div.innerHTML = "<p>Failed to load contest data.</p>";
+            }
         });
     });
-}
+})();
